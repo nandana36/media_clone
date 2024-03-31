2cf34ebd69010d190e66c02907899e1268e018c7
Nandana S <nxndhx.srdvi36@gmail.com>
2024-03-31 10:48:59 +0530

    Uploaded

---
+++ src/components/Loader.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import loader from './loader.gif'
+const Loader = () => {
+  return (
+    <div className="text-center">
+      <img src={loader} alt="loading" />
+    </div>
+  )
+}
+
+export default Loader
---
+++ src/utils/constants.js
@@ -0,0 +1,26 @@
+import MusicNoteIcon from '@mui/icons-material/MusicNote';
+import HomeIcon from '@mui/icons-material/Home';
+import CodeIcon from '@mui/icons-material/Code';
+
+import SportsEsportsIcon from '@mui/icons-material/SportsEsports';
+
+import FitnessCenterIcon from '@mui/icons-material/FitnessCenter';
+
+
+export const logo = 'https://i.ibb.co/s9Qys2j/logo.png';
+
+export const categories = [
+  { name: 'All', icon: <HomeIcon />, },  
+  { name: 'Coding', icon: <CodeIcon />, },  
+  { name: 'Music', icon: <MusicNoteIcon /> },  
+  { name: 'Gaming', icon: <SportsEsportsIcon />, },  
+  { name: 'Sport', icon: <FitnessCenterIcon />, },
+  
+];
+
+export const demoThumbnailUrl = 'https://i.ibb.co/G2L2Gwp/API-Course.png';
+export const demoChannelUrl = '/channel/UCmXmlB4-HJytD7wek0Uo97A';
+export const demoVideoUrl = '/video/GDa8kZLNhJ4';
+export const demoChannelTitle = 'JavaScript Mastery';
+export const demoVideoTitle = 'Build and Deploy 5 JavaScript & React API Projects in 10 Hours - Full Course | RapidAPI';
+export const demoProfilePicture = 'http://dergipark.org.tr/assets/app/images/buddy_sample.png'
